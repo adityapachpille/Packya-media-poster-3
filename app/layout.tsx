d1276aa2368b36304f697745a7c4d7469e773898
adityapachpille <158599059+adityapachpille@users.noreply.github.com>
2025-11-27 20:22:41 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,13 +5,6 @@ import Footer from "@/components/Footer";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
-});import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-import Footer from "@/components/Footer";
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
@@ -21,34 +14,7 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "LokNetaa Poster Editor Software",
-  description: "Design packy Media Services.",
-};
-
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-        <Footer/>
-      </body>
-    </html>
-  );
-}
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-export const metadata: Metadata = {
-  title: "LokNetaa Poster Editor Software",
-  description: "Develop By ARP Digital Solution.",
+  description: "Develop By ARP Digital Solutions.",
 };
 
 export default function RootLayout({

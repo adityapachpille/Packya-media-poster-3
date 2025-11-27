6a569e10e3848185615e9bdc6064dfcfdec5da63
adityapachpille <158599059+adityapachpille@users.noreply.github.com>
2025-11-27 21:29:45 +0530

    Update CanvasEditor.jsx

--- components/CanvasEditor.jsx
+++ components/CanvasEditor.jsx
@@ -14,8 +14,8 @@ export default function CanvasEditor() {
   const [scale, setScale] = useState(1);
 
   const [position, setPosition] = useState({
-    x: 40,
-    y: canvasSize / 2 - imageSize / 2 + 0,
+    x: 44,
+    y: canvasSize / 2 - imageSize / 2 - 10,
   });
 
   const handleUpload = (e) => {
@@ -115,7 +115,7 @@ export default function CanvasEditor() {
     <>
       {/* Top bar */}
       <div className="topBarWrapper">
-        <h2 className="title">📲🗳️ LokNetaa Poster Editor Softwaree 👥📸✨</h2>
+        <h2 className="title">📲🗳️ LokNetaa Poster Editor Softwareee 👥📸✨</h2>
         <div className="topBar">
           <button className="button" onClick={() => fileRef.current?.click()}>
             📤 Import Photo
